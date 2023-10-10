541eb0b5757ba4dff0ead1c7d1adfa41c1a058ea
igorpancheenko <igorpanchenko94@yandex.ru>
2023-10-10 17:02:52 +0500

    startprojectbash: строка 1: begined: команда не найдена

---
+++ books/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Название')
+    price = models.DecimalField(max_digits=7, decimal_places=2)
